95e874039207dec7da271750f6a39a306f124a49
Daniil Hodanovich <Dackeron123@gmail.com>
2026-03-25 12:30:52 +0700

    v0.7.7
    Add:
     - Lens Dance Animation
     - Error Dance Animation
     - DragDrop OST
     - Waltz OST
    Rework:
     - Main menu
    Fix:
     - Big logo

---
+++ Assets/Scripts/Buttons/MainMenuScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Video;
+public class MainMenuScript : MonoBehaviour{
+    [SerializeField] VideoPlayer Video;
+    [SerializeField] AudioSource src;
+
+    [SerializeField] AudioClip normal_music;
+    [SerializeField] AudioClip error_music;
+
+    [SerializeField] VideoClip lens;
+    [SerializeField] VideoClip error;
+    int i;
+
+    void Start(){
+        i = Random.Range(1, 11);
+        if(i == 7){
+            Video.clip = error;
+            src.clip = error_music;
+        }
+        else
+        {
+            Video.clip = lens;
+            src.clip = normal_music;
+        }
+        Video.Play();
+        src.Play();
+    }
+}
