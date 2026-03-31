af94d6c2414ccc0f2473df8835b49711faead1a9
Daniil Hodanovich <Dackeron123@gmail.com>
2026-03-31 10:56:00 +0700

    v0.8.7
    Fix:
     - Front end bug in build
     - UI resize in build

--- Assets/Scripts/FrontEnd/ConnectionLine.cs
+++ Assets/Scripts/FrontEnd/ConnectionLine.cs
@@ -6,24 +6,56 @@ public class ConnectionLine : MonoBehaviour
     public RectTransform a;
     public RectTransform b;
     public bool followMouse = false;
+    public Canvas canvas;
 
     private void Awake()
-    {
-        rect = GetComponent<RectTransform>();
-    }
+{
+    rect = GetComponent<RectTransform>();
+    canvas = GetComponentInParent<Canvas>();
+}
 
     private void Update()
+{
+    if (a == null) return;
+
+    Vector2 posA, posB;
+
+    RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+
+    // A
+    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+        canvasRect,
+        RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, a.position),
+        canvas.worldCamera,
+        out posA
+    );
+
+    // B
+    if (followMouse)
     {
-        if (a == null) return;
-
-        Vector2 posA = a.position;
-        Vector2 posB = followMouse ? Input.mousePosition : (b != null ? b.position : posA);
-
-        Vector2 dir = posB - posA;
-        rect.position = posA + dir / 2f;
-        rect.sizeDelta = new Vector2(dir.magnitude, 5f);
-
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        rect.rotation = Quaternion.Euler(0, 0, angle);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRect,
+            Input.mousePosition,
+            canvas.worldCamera,
+            out posB
+        );
     }
+    else
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRect,
+            RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, b.position),
+            canvas.worldCamera,
+            out posB
+        );
+    }
+
+    Vector2 dir = posB - posA;
+
+    rect.anchoredPosition = posA + dir / 2f;
+    rect.sizeDelta = new Vector2(dir.magnitude, 5f);
+
+    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    rect.localRotation = Quaternion.Euler(0, 0, angle);
+}
 }
