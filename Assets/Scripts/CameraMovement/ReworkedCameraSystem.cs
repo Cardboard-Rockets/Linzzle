6ca2590279eaa3f514a7f018a856bc3f7234a3b3
Daniil Hodanovich <Dackeron123@gmail.com>
2026-04-10 10:46:49 +0700

    v1.1
    Add:
     - Splash loading screen
     - GitHub open button in main menu
    Update:
     - Camera now moving using dragclick

--- Assets/Scripts/CameraMovement/ReworkedCameraSystem.cs
+++ Assets/Scripts/CameraMovement/ReworkedCameraSystem.cs
@@ -3,15 +3,6 @@ using UnityEngine.EventSystems;
 
 public class ReworkedCameraSystem : MonoBehaviour
 {
-    public float speed = 15f;
-
-    public float leftBorder = 20f;
-    public float rightBorder = 20f;
-    public float topBorder = 120f;
-    public float bottomBorder = 120f;
-
-    public float verticalDelay = 0.25f;
-    float verticalTimer = 0;
 
     [SerializeField] float minX;
     [SerializeField] float maxX;
@@ -29,41 +20,6 @@ public class ReworkedCameraSystem : MonoBehaviour
 
     void Update()
     {
-        if (EventSystem.current.IsPointerOverGameObject())
-            return;
-
-        Vector3 move = Vector3.zero;
-
-        if (Input.mousePosition.x <= leftBorder)
-        {
-            move.x = -1;
-        }
-
-        if (Input.mousePosition.x >= Screen.width - rightBorder)
-        {
-            move.x = 1;
-        }
-
-        if (Input.mousePosition.y <= bottomBorder)
-        {
-            verticalTimer += Time.deltaTime;
-
-            if (verticalTimer >= verticalDelay)
-                move.y = -1;
-        }
-        else if (Input.mousePosition.y >= Screen.height - topBorder)
-        {
-            verticalTimer += Time.deltaTime;
-
-            if (verticalTimer >= verticalDelay)
-                move.y = 1;
-        }
-        else
-        {
-            verticalTimer = 0;
-        }
-        transform.position += move * speed * Time.deltaTime;
-
         if(borderSwitch){
         CameraBorder();
         }
