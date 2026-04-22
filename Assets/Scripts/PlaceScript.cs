03e2d20a11c86fe78d3cad64eb5dd90f98742bb6
Daniil Hodanovich <Dackeron123@gmail.com>
2026-04-22 09:01:08 +0700

    v1.5.4.3
    Fix:
     - Place script

--- Assets/Scripts/PlaceScript.cs
+++ Assets/Scripts/PlaceScript.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
-using static UnityEditor.PlayerSettings;
 
 public class PlaceScript : MonoBehaviour
 {
